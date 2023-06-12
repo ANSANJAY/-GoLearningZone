f00fe665fd3271edb1090dc40e46981d537d7a1d
AnamikaSanjay <asanjay@intelops.dev>
2023-06-12 12:19:40 -0400

    added comment

--- mange_simple_variable_values/get_inpt/main.go
+++ mange_simple_variable_values/get_inpt/main.go
@@ -10,8 +10,8 @@ import(
 
 func main(){
 
-reader := bufio.NewReader(os.Stdin)
-fmt.Print("Enter text: ")
-input , _ := reader.ReadString('\n')
+reader := bufio.NewReader(os.Stdin) // new variable
+fmt.Print("Enter text: ") // promt to user
+input , _ := reader.ReadString('\n') //if you have to ignore vairable , name _
 fmt.Println("given value:",input)
 }
